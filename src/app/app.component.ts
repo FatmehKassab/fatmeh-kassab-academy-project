5d5b205645d3612db09e6eabe46dd4a113daf209
Fatmeh Kassab <f.kassab@specutech.com>
2025-07-09 22:57:04 +0300

    feat: created button and text input dynamic components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TextInputComponent } from "./shared/components/inputs/text-input/text-input.component";
+import { ButtonComponent } from "./shared/components/button/button.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TextInputComponent, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'fatmeh-kassab-academy-project';
+   username: string = '';
+  password: string = '';
+  email: string = '';
+  
+  handleSubmit(): void {
+  console.log('Button clicked!');
 }
+
+}
+
