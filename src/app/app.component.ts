bbb2d72475f51e2da7a2911f70d997a9ef06ad16
Fatmeh Kassab <f.kassab@specutech.com>
2025-07-10 21:48:52 +0300

    feat: create sign in page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { TextInputComponent } from "./shared/components/inputs/text-input/text-input.component";
 import { ButtonComponent } from "./shared/components/button/button.component";
 import { SignUpComponent } from "./pages/sign-up/sign-up.component";
+import { SignInComponent } from "./pages/sign-in/sign-in.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SignUpComponent],
+  imports: [RouterOutlet, SignUpComponent, SignInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
