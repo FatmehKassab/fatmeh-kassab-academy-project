096c50cad41e8d3fb9ed535d3947b0bd14c9ee11
Fatmeh Kassab <f.kassab@specutech.com>
2025-07-15 12:44:50 +0300

    feat: create admin page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { AboutComponent } from './pages/about/about.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 
 export const routes: Routes = [
+  { path: 'admin', component: AdminComponent, data: { title: 'Admin' } },
   { path: 'about', component: AboutComponent, data: { title: 'About' } },
   { path: 'products', component: ProductsComponent, data: { title: 'Products' } },
   { path: 'sign-in', component: SignInComponent, data: { title: 'Sign In' } },
