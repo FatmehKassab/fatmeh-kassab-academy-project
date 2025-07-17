0058c6283388fe0818b1e5a1149ed8a1be2ed7fc
Fatmeh Kassab <f.kassab@specutech.com>
2025-07-18 00:20:42 +0300

    fix: fix conflict

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -4,14 +4,14 @@ import { ProductCardComponent } from "../../shared/components/product-card/produ
 import { BooleanInputComponent } from "../../shared/components/inputs/boolean-input/boolean-input.component";
 import { Product, ProductService } from '../../shared/services/product.service';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { FooterComponent } from "../../core/layout/footer/footer.component";
 
 @Component({
   selector: 'app-products',
 
   standalone:true,
-  imports: [NavbarComponent, ProductCardComponent, CommonModule, BooleanInputComponent, FooterComponent],
+  imports: [NavbarComponent, ProductCardComponent, CommonModule, BooleanInputComponent, FooterComponent,FormsModule],
 
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
