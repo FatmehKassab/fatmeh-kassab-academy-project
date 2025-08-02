8a2c29115f7d0b8247c3fda0f626a0f9e134f8b1
Fatmeh Kassab <f.kassab@specutech.com>
2025-08-02 23:46:19 +0300

    feat: add home images

--- src/app/shared/utils/images.ts
+++ src/app/shared/utils/images.ts
@@ -1,4 +1,12 @@
 export const IMAGES = {
+  earrings: 'images/earrings.png',
+  headphones: 'images/headphones.png',
   login_illustration: 'images/login_illustration.svg',
   logo: 'images/logo.svg',
+  man: 'images/man.png',
+  mantop: 'images/mantop.png',
+  ring: 'images/ring.png',
+  tv: 'images/tv.png',
+  woman: 'images/woman.png',
+  womantop: 'images/womantop.png'
 };
