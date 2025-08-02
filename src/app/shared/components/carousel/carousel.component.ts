1e131985216896bdcf346beacfc7b0aa909c0a28
Fatmeh Kassab <f.kassab@specutech.com>
2025-08-03 00:13:54 +0300

    fix: fix images position

--- src/app/shared/components/carousel/carousel.component.ts
+++ src/app/shared/components/carousel/carousel.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CarouselModule } from 'primeng/carousel';
+import { IMAGES } from '../../utils/images';
 @Component({
   selector: 'app-carousel',
   imports: [CarouselModule],
@@ -7,5 +8,22 @@ import { CarouselModule } from 'primeng/carousel';
   styleUrl: './carousel.component.scss'
 })
 export class CarouselComponent {
-
+IMAGES =IMAGES;
+slides = [
+    {
+      title: 'Style, Tech & Beyond',
+      description: 'Own the moment and redefine your style with ModaFusion. New arrivals that slay — shop now!',
+      button: 'Start Shopping'
+    },
+    {
+      title: 'Refined Looks for Him',
+      description: 'Dress sharp and own every moment — curated style just for you.',
+      button: 'Explore Men\'s Collection'
+    },
+    {
+      title: 'Empowered Elegance',
+      description: 'Elegance redefined for the modern woman.',
+      button: 'Shop Now'
+    }
+  ];
 }
