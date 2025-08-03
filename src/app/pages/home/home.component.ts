75dd6450ae718a878f50a102676fe2d5ed24e03d
Fatmeh Kassab <f.kassab@specutech.com>
2025-08-04 00:51:02 +0300

    feat: create cta section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../core/layout/navbar/navbar.component';
 import { CarouselComponent } from '../../shared/components/carousel/carousel.component';
 import { CategoriesComponent } from "../../shared/components/categories/categories.component";
+import { CtaComponent } from '../../shared/components/cta/cta.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CarouselComponent, CategoriesComponent],
+  imports: [CarouselComponent, CategoriesComponent,CtaComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
