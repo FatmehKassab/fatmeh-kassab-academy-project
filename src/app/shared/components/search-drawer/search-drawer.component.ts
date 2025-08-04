09f86c32e2fd382b4d34731c1d89452df8111a10
Fatmeh Kassab <f.kassab@specutech.com>
2025-08-04 22:45:08 +0300

    feat: search is working in the modal

--- src/app/shared/components/search-drawer/search-drawer.component.ts
+++ src/app/shared/components/search-drawer/search-drawer.component.ts
@@ -1,35 +1,53 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { DrawerModule } from 'primeng/drawer';
-import { SearchDrawerService } from '../../services/searchDrawer.service';
 import { DialogModule } from 'primeng/dialog';
-import { SearchInputComponent } from "../inputs/search-input/search-input.component";
+import { SearchInputComponent } from '../inputs/search-input/search-input.component';
+import { SearchDrawerService } from '../../services/searchDrawer.service';
+
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../interfaces/product.model';
+import { ProductCardComponent } from "../product-card/product-card.component";
 
 @Component({
   selector: 'app-search-drawer',
-  imports: [DrawerModule, DialogModule, SearchInputComponent],
+  standalone: true,
+  imports: [DrawerModule, DialogModule, SearchInputComponent, CommonModule, ProductCardComponent],
   templateUrl: './search-drawer.component.html',
-  styleUrl: './search-drawer.component.scss'
+  styleUrl: './search-drawer.component.scss',
 })
 export class SearchDrawerComponent implements OnInit {
   visible = false;
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
 
-    constructor(
-private searchDrawerService : SearchDrawerService
-  ) {
-    
-  }
-
+  constructor(
+    private searchDrawerService: SearchDrawerService,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
-    console.log("drawerrrr")
-    this.searchDrawerService.drawerVisible$.subscribe(visible => {
-      this.visible = visible;
-    });
+  this.searchDrawerService.drawerVisible$.subscribe((visible) => {
+    this.visible = visible;
+  });
+
+  this.productService.getAllProducts().subscribe((products) => {
+    this.products = products;
+    this.filteredProducts = [];
+  });
+}
+
+onSearch(query: string) {
+  if (!query.trim()) {
+    this.filteredProducts = [];
+    return;
   }
 
-  onSearch(query: string) {
-  console.log('Search:', query);
-  // filter or fetch results
+  this.filteredProducts = this.products.filter((product) =>
+  product.title.toLowerCase().includes(query.toLowerCase()) ||
+    product.category.toLowerCase().includes(query.toLowerCase())
+  
+  );
 }
 
 }
