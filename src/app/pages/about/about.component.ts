2308ab12e63ccdddba71f24a58107b77641cd60d
Fatmeh Kassab <f.kassab@specutech.com>
2025-08-06 17:53:48 +0300

    feat: create about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { IMAGES } from '../../shared/utils/images';
+import { CtaComponent } from "../../shared/components/cta/cta.component";
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CtaComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+IMAGES=IMAGES
 }
