4f28c7d47e2be59cb6e2b0fedf9d6fbfdaa578c4
Fatmeh Kassab <f.kassab@specutech.com>
2025-08-07 22:14:05 +0300

    fix: fix the counter in the product detail page

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Product } from '../../shared/interfaces/product.model';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../shared/services/product.service';
-import { CommonModule, NgIf } from '@angular/common';
+import { AsyncPipe, CommonModule, NgIf } from '@angular/common';
 import { TitleService } from '../../shared/services/title.service';
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
@@ -14,16 +14,18 @@ import { FavoritesService } from '../../shared/services/favorites.service';
 import { SocialsComponent } from '../../shared/components/socials/socials.component';
 import { CounterComponent } from '../../shared/components/counter/counter.component';
 import { Store } from '@ngrx/store';
-import { selectCartItems } from '../../shared/store/cart/cart.selectors';
+import { selectCartItems, selectQuantityByProductId } from '../../shared/store/cart/cart.selectors';
 import { ProgressBarModule } from 'primeng/progressbar';
 import { ExploreComponent } from '../../shared/components/explore/explore.component';
+import { addToCart } from '../../shared/store/cart/cart.actions';
+import { Observable } from 'rxjs';
 
 
 @Component({
   selector: 'app-product-detail',
   standalone:true,
   imports: [NgIf,RatingModule,CommonModule, FormsModule, AccordionModule, ButtonComponent,SocialsComponent,CounterComponent, ProgressBarModule,
-    ExploreComponent],
+    ExploreComponent,AsyncPipe],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss'
 })
@@ -34,6 +36,8 @@ export class ProductDetailComponent implements OnInit {
   selectedSize: string = '';
  ICONS = ICONS;
  @Input() products: any[] = [];
+ productQuantity$!: Observable<number>;
+   @Input() productId!: number;
 private favoritesService = inject(FavoritesService);
   
   constructor(
@@ -45,6 +49,9 @@ private favoritesService = inject(FavoritesService);
   ) {}
 
 
+ addToCart(product: any) {
+  this.store.dispatch(addToCart({ product }));
+}
 
 selectSize(size: string) {
   this.selectedSize = size;
@@ -72,6 +79,11 @@ ngOnInit(): void {
     
         window.scrollTo({ top: 0, behavior: 'smooth' });
       });
+       this.productService.getProductById(id).subscribe(product => {
+  this.product = product;
+  this.productId = product.id; 
+  this.productQuantity$ = this.store.select(selectQuantityByProductId(this.productId)); 
+});
     }
   });
 
@@ -83,8 +95,11 @@ ngOnInit(): void {
   this.store.select(selectCartItems).subscribe(items => {
     this.products = [...items];
   });
-}
 
+ 
+
+ 
+}
 
 
   
